921b001d21aa5944c02301754a64a07daa235daa
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-02-28 23:18:11 +0900

    feat : Home UI 적용

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/home/HomeFragment.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/home/HomeFragment.kt
@@ -1,17 +1,22 @@
 package com.side.runwithme.view.home
 
-import androidx.navigation.fragment.findNavController
+import com.example.seobaseview.base.BaseFragment
 import com.side.runwithme.R
-import com.side.runwithme.base.BaseFragment
 import com.side.runwithme.databinding.FragmentHomeBinding
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override fun init() {
-//        binding.toolbarMain.setBackButtonClickEvent {
-//            findNavController().popBackStack()
-//        }
+
+        initClickListener()
+
+    }
+
+    private fun initClickListener(){
+        binding.apply {
+
+        }
     }
 
 
