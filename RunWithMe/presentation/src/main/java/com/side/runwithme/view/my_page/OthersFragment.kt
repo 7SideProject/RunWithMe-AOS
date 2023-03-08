a2124f9ebee8bf62b1c38503b27518dbd1f5b85e
hyeonjin0103 <hyeonjin0103@o.cnu.ac.kr>
2023-03-09 00:15:14 +0900

    feat : myPage ui 일괄 추가

---
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/my_page/OthersFragment.kt
@@ -0,0 +1,22 @@
+package com.side.runwithme.view.my_page
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.seobaseview.base.BaseFragment
+import com.side.runwithme.R
+import com.side.runwithme.databinding.FragmentHomeBinding
+
+class OthersFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_others) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
