40b493f30f747c20c7b7502abaaf39c535daf93a
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-03-20 15:39:27 +0900

    feat : RouteDetail Map 추가

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/route_detail/RouteDetailFragment.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/route_detail/RouteDetailFragment.kt
@@ -8,12 +8,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.seobaseview.base.BaseFragment
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 import com.side.runwithme.R
 import com.side.runwithme.databinding.FragmentRouteDetailBinding
+import com.side.runwithme.util.LOCATION_PERMISSION_REQUEST_CODE
 
-class RouteDetailFragment : BaseFragment<FragmentRouteDetailBinding>(R.layout.fragment_route_detail) {
+class RouteDetailFragment : BaseFragment<FragmentRouteDetailBinding>(R.layout.fragment_route_detail), OnMapReadyCallback {
+
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
 
     override fun init() {
+
+        initMapView()
+
+        initClickListener()
 
     }
 
@@ -24,6 +38,29 @@ class RouteDetailFragment : BaseFragment<FragmentRouteDetailBinding>(R.layout.fr
             }
 
         }
+    }
+
+    private fun initMapView(){
+        val fm = activity?.supportFragmentManager
+        val mapFragment = fm!!.findFragmentById(R.id.map_view) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm!!.beginTransaction().add(R.id.map_view, it).commit()
+            }
+        mapFragment.getMapAsync(this)
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        // 라이트 모드 설정 시 지도 심벌의 클릭 이벤트를 처리할 수 없습니다
+        this.naverMap = naverMap
+        naverMap.moveCamera(CameraUpdate.zoomTo(16.0))
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        naverMap.isLiteModeEnabled = true
+        // 현위치 버튼 활성화
+        naverMap.uiSettings.isLocationButtonEnabled = true
+
     }
 
 }
