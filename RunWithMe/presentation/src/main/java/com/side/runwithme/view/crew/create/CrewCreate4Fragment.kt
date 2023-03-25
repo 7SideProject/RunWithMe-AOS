18acc27bb93100eff967f79cd15df0747eb6deb9
JeongBJ <ebjdev@gmail.com>
2023-03-26 01:33:28 +0900

    feat : 크루 생성 UI

---
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/crew/create/CrewCreate4Fragment.kt
@@ -0,0 +1,11 @@
+package com.side.runwithme.view.crew.create
+
+import com.example.seobaseview.base.BaseFragment
+import com.side.runwithme.R
+import com.side.runwithme.databinding.FragmentCrewCreate4Binding
+
+class CrewCreate4Fragment : BaseFragment<FragmentCrewCreate4Binding>(R.layout.fragment_crew_create4) {
+    override fun init() {
+
+    }
+}
