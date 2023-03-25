d0c037e550b82a14df988657bc8608c5c85bde74
JeongBJ <ebjdev@gmail.com>
2023-03-26 01:34:12 +0900

    feat : 크루 정보 UI

---
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/crew/detail/CrewDetailFragment.kt
@@ -0,0 +1,11 @@
+package com.side.runwithme.view.crew.detail
+
+import com.example.seobaseview.base.BaseFragment
+import com.side.runwithme.R
+import com.side.runwithme.databinding.FragmentCrewDetailBinding
+
+class CrewDetailFragment : BaseFragment<FragmentCrewDetailBinding>(R.layout.fragment_crew_detail) {
+    override fun init() {
+
+    }
+}
