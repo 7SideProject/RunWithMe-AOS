121b1cab0ad6120ac30bbbdea8ff4f23b0d36216
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-04-02 22:42:21 +0900

    feat : JoinActivity 추가

---
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/join/JoinActivity.kt
@@ -0,0 +1,13 @@
+package com.side.runwithme.view.join
+
+import com.example.seobaseview.base.BaseActivity
+import com.side.runwithme.R
+import com.side.runwithme.databinding.ActivityJoinBinding
+
+class JoinActivity : BaseActivity<ActivityJoinBinding>(R.layout.activity_join) {
+
+    override fun init() {
+
+    }
+
+}
