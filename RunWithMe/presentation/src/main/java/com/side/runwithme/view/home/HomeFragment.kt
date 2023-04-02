17d65e9a453bd60a3cc3b22ef43cef712a334ee6
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-04-02 15:33:11 +0900

    test : ktlint

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/home/HomeFragment.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/home/HomeFragment.kt
@@ -1,14 +1,17 @@
 package com.side.runwithme.view.home
 
 
+import android.accounts.OnAccountsUpdateListener
+import android.app.UiAutomation.OnAccessibilityEventListener
+import android.view.MenuItem.OnActionExpandListener
 import com.example.seobaseview.base.BaseFragment
 import androidx.navigation.fragment.findNavController
+import com.naver.maps.map.OnMapReadyCallback
 import com.side.runwithme.R
-import com.side.runwithme.base.BaseFragment
 import com.side.runwithme.databinding.FragmentHomeBinding
 
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home), OnMapReadyCallback, OnAccessibilityEventListener, OnAccountsUpdateListener, OnActionExpandListener {
 
     override fun init() {
 
