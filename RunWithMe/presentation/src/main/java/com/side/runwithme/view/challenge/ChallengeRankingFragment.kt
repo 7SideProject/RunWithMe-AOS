1ca1391cdc206a0e58422316ed174aa5bfe11676
SANG\jdsae <jdsaeyqi@naver.com>
2023-04-10 13:59:13 +0900

    feat : 챌린지 랭킹 UI

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/challenge/ChallengeRankingFragment.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/challenge/ChallengeRankingFragment.kt
@@ -4,8 +4,7 @@ import com.example.seobaseview.base.BaseFragment
 import com.side.runwithme.R
 import com.side.runwithme.databinding.FragmentChallengeRankingBinding
 
-class ChallengeRankingFragment :
-    BaseFragment<FragmentChallengeRankingBinding>(R.layout.fragment_challenge_ranking) {
+class ChallengeRankingFragment : BaseFragment<FragmentChallengeRankingBinding>(R.layout.fragment_challenge_ranking) {
     override fun init() {
 
     }
