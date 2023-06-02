14ebb167c39f8c66d37482fe63e0ef4e18aebaf4
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-06-02 18:04:07 +0900

    feat : dialog extension 다이얼로그 사이즈 조절

--- RunWithMe/presentation/src/main/java/com/side/runwithme/util/Extensions.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/util/Extensions.kt
@@ -1,9 +1,11 @@
 package com.side.runwithme.util
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.Context
 import android.graphics.Insets
 import android.graphics.Point
+import android.os.Build
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.datastore.core.DataStore
@@ -24,6 +26,34 @@ import java.io.IOException
 fun getDeviceSize(activity: Activity): Point {
     val windowManager = activity.getSystemService(Context.WINDOW_SERVICE) as WindowManager
     return windowManager.currentWindowMetricsPointCompat()
+}
+
+// 다이얼로그 사이즈 조절
+fun Context.dialogResize(dialog: Dialog, width: Float, height: Float){
+    val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+    if (Build.VERSION.SDK_INT < 30){
+        val display = windowManager.defaultDisplay
+        val size = Point()
+
+        display.getSize(size)
+
+        val window = dialog.window
+
+        val x = (size.x * width).toInt()
+        val y = (size.y * height).toInt()
+
+        window?.setLayout(x, y)
+
+    }else{
+        val rect = windowManager.currentWindowMetrics.bounds
+
+        val window = dialog.window
+        val x = (rect.width() * width).toInt()
+        val y = (rect.height() * height).toInt()
+
+        window?.setLayout(x, y)
+    }
 }
 
 fun WindowManager.currentWindowMetricsPointCompat(): Point {
