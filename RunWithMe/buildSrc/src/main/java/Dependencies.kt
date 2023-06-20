f9429f760af93ffc5adda4f74a5e48282c0b8124
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-06-20 23:36:15 +0900

    feat : buildSrc 파일

---
+++ RunWithMe/buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,57 @@
+
+object AndroidX {
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT}"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:${Versions.APP_COMPAT}"
+    const val METERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
+    const val LEGACY_SUPPORT = "androidx.legacy:legacy-support-v4:${Versions.LEGACY_SUPPORT}"
+    const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.VIEWMODEL}"
+}
+
+object Ktx {
+    const val CORE = "androidx.core:core-ktx:${Versions.CORE_KTX}"
+}
+
+object Test {
+    const val JUNIT = "junit:junit:${Versions.JUNIT4}"
+    const val ANDROID_JUNIT = "androidx.test.ext:junit:${Versions.ANDROID_JUNIT}"
+    const val ANDROID_TRUTH = "androidx.test.ext:truth:${Versions.ANDROID_TRUTH}"
+    const val ANDROID_RUNNER = "androidx.test:runner:${Versions.ANDROID_RUNNER}"
+    const val ANDROID_MOCK = "io.mockk:mockk-android:${Versions.ANDROID_MOCK}"
+    const val AGENT_MOCK = "io.mockk:mockk-agent:${Versions.ANDROID_MOCK}"
+    const val COROUTINE_TEST = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.COROUTINE_TEST}"
+    const val HILT_TEST = "com.google.dagger:hilt-android-testing:${Versions.HILT_TEST}"
+    const val KAPT_HILT_TEST = "com.google.dagger:hilt-android-compiler:${Versions.HILT_TEST}"
+    const val SLF4J_SIMPLE = "org.slf4j:slf4j-simple:${Versions.SLF4J_SIMPLE}"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO_CORE}"
+}
+
+object Dependencies {
+    const val LIVE_DATA = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.LIVEDATA}"
+    const val DAGGER_HILT = "com.google.dagger:hilt-android:${Versions.HILT}"
+    const val DAGGER_HILT_CORE = "com.google.dagger:hilt-core:${Versions.HILT}"
+    const val KAPT_HILT = "com.google.dagger:hilt-android-compiler:${Versions.HILT}"
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}"
+    const val GSON = "com.squareup.retrofit2:converter-gson:${Versions.GSON}"
+    const val OKHTTP = "com.squareup.okhttp3:okhttp:${Versions.OKHTTP}"
+    const val OKHTTP_LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP}"
+    const val NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:${Versions.NAVIGATION}"
+    const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:${Versions.NAVIGATION}"
+    const val COROUTINE_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.COROUTINE_ANDROID}"
+    const val COROUTINE_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINE_ANDROID}"
+    const val TED_PERMISSION_NORMAL = "io.github.ParkSangGwon:tedpermission-normal:${Versions.TED_PERMISSION}"
+    const val GLIDE = "com.github.bumptech.glide:glide:${Versions.GLIDE}"
+    const val KAPT_GLIDE = "com.github.bumptech.glide:compiler:${Versions.GLIDE}"
+    const val KAKAO_USER = "com.kakao.sdk:v2-user:${Versions.KAKAO_USER}"
+    const val SEOBASEVIEW = "com.github.skw4223:SeoBaseView:${Versions.SEOBASEVIEW}"
+    const val NAVER_MAP = "com.naver.maps:map-sdk:${Versions.NAVER_MAP}"
+    const val GOOGLE_LOCATION = "com.google.android.gms:play-services-location:${Versions.GOOGLE_LOCATION}"
+    const val SIMPLE_RATING_BAR = "com.github.ome450901:SimpleRatingBar:${Versions.SIMPLE_RATING_BAR}"
+    const val LOTTIE = "com.airbnb.android:lottie:${Versions.LOTTIE}"
+    const val CIRCLE_IMAGE_VIEW = "de.hdodenhof:circleimageview:${Versions.CIRCLE_IMAGE_VIEW}"
+    const val CALENDAR_VIEW = "com.github.kizitonwose:CalendarView:${Versions.CALENDAR_VIEW}"
+    const val MP_ANDROID_CHART = "com.github.PhilJay:MPAndroidChart:v${Versions.MP_ANDROID_CHART}"
+    const val DATASTORE_PREFERENCES = "androidx.datastore:datastore-preferences:${Versions.DATASTORE_PREFERENCES}"
+    const val DESUGARING = "com.android.tools:desugar_jdk_libs:${Versions.DESUGARING}"
+    const val PAGING = "androidx.paging:paging-runtime-ktx:${Versions.PAGING}"
+    const val PAGING_COMMON = "androidx.paging:paging-common-ktx:${Versions.PAGING}"
+}
