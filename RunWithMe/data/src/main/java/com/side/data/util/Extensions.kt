4ce7a94b219712646acea01817c91d316772a449
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-07-03 20:30:20 +0900

    feat : emitLoading, emitSuccess, emitFailr 확장 함수 추가

--- RunWithMe/data/src/main/java/com/side/data/util/Extensions.kt
+++ RunWithMe/data/src/main/java/com/side/data/util/Extensions.kt
@@ -7,3 +7,15 @@ import kotlinx.coroutines.flow.FlowCollector
 suspend fun <T> FlowCollector<ResultType<BaseResponse<T>>>.emitResultTypeError(throwable: Throwable) {
     this.emit(ResultType.Error(throwable))
 }
+
+suspend fun FlowCollector<ResultType.Loading>.emitResultTypeLoading() {
+    this.emit(ResultType.Loading)
+}
+
+suspend fun <T> FlowCollector<ResultType<T>>.emitResultTypeSuccess(data: T) {
+    this.emit(ResultType.Success(data))
+}
+
+suspend fun <T> FlowCollector<ResultType<T>>.emitResultTypeFail(data: T) {
+    this.emit(ResultType.Fail(data))
+}
