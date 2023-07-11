5ddbcec81020baa3806058baa9465c86a8b1635c
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-07-11 20:03:27 +0900

    refact : bindingadapter 수정

--- RunWithMe/presentation/src/main/java/com/side/runwithme/binding/RecyclerBinding.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/binding/RecyclerBinding.kt
@@ -10,29 +10,24 @@ import com.side.runwithme.view.challenge.ChallengeListAdapter
 import com.side.runwithme.view.running_list.RunningListAdapter
 import kotlinx.coroutines.flow.MutableStateFlow
 
-object RecyclerBinding {
 
-    @BindingAdapter("submitList")
-    @JvmStatic
-    fun bindSubmitList(view : RecyclerView, result : ResultType<*>?){
-        if(result is ResultType.Success){
-            when(view.adapter){
-                is ChallengeListAdapter -> {
-                    Log.d("test123", "bindSubmitList: ")
-                    (view.adapter as ListAdapter<Any,*>).submitList(result.data as List<Challenge>)
-                }
+@BindingAdapter("submitList")
+fun bindSubmitList(view: RecyclerView, result: ResultType<*>?) {
+    if (result is ResultType.Success) {
+        when (view.adapter) {
+            is ChallengeListAdapter -> {
+                (view.adapter as ListAdapter<Any, *>).submitList(result.data as List<Challenge>)
             }
         }
     }
+}
 
-    @BindingAdapter("submitList2")
-    @JvmStatic
-    fun bindSubmitList(view : RecyclerView, result : List<*>?){
-        when(view.adapter){
-            is RunningListAdapter -> {
-                (view.adapter as ListAdapter<Any,*>).submitList(result!! as List<Challenge>)
-            }
+@BindingAdapter("submitList2")
+fun bindSubmitList(view: RecyclerView, result: List<*>?) {
+    when (view.adapter) {
+        is RunningListAdapter -> {
+            (view.adapter as ListAdapter<Any, *>).submitList(result!! as List<Challenge>)
         }
-
     }
+
 }
--- RunWithMe/presentation/src/main/java/com/side/runwithme/binding/ViewBindingAdapter.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/binding/ViewBindingAdapter.kt
@@ -5,84 +5,73 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.side.runwithme.R
-import com.side.runwithme.binding.ViewBindingAdapter.setRunningResultCompleted
-import com.willy.ratingbar.ScaleRatingBar
 import okhttp3.MultipartBody
 import java.lang.Math.round
 
-object ViewBindingAdapter {
 
-    @BindingAdapter("runRecordImgFile")
-    @JvmStatic
-    fun ImageView.setRunRecordImgFile(imgFile: MultipartBody.Part) {
+@BindingAdapter("runRecordImgFile")
+fun ImageView.setRunRecordImgFile(imgFile: MultipartBody.Part) {
+    Glide.with(this.context)
+        .load(imgFile)
+        .into(this)
+}
+
+
+
+@BindingAdapter("runningResultDay")
+fun TextView.setRunningResultDay(day: String) {
+    val date = day.split("-")
+    val year = date.get(0)
+    val month = date.get(1).toInt()
+    val day = date.get(2).toInt()
+
+    this.text = resources.getString(R.string.running_result_day, year, month, day)
+}
+
+@BindingAdapter("runningDistance")
+fun TextView.setRunnignDistance(distance: Int) {
+    this.text = "${distance / 1000} km"
+}
+
+@BindingAdapter("runningTime")
+fun TextView.setRunningTime(time: Int) {
+    val hourInt = time / 3600
+    val minuteInt = (time / 60) % 60
+
+    var hour = hourInt.toString()
+    if (hourInt < 10) {
+        hour = "0" + hour
+    }
+
+    var minute = minuteInt.toString()
+    if (minuteInt < 10) {
+        minute = "0" + minute
+    }
+
+    this.text = "${hour} : ${minute}"
+}
+
+@BindingAdapter("runningAvgSpeed")
+fun TextView.setRunningAvgSpeed(avgSpeed: Double) {
+    this.text = "${round(avgSpeed * 10.0) / 10.0} km/h"
+}
+
+@BindingAdapter("runningStartTime", "runningEndTime")
+fun TextView.setRunningStartToEndTime(startTime: String, endTime: String) {
+    this.text = "$startTime ~ $endTime"
+}
+
+@BindingAdapter("runningResultCompleted")
+fun ImageView.setRunningResultCompleted(completed: String) {
+    if (completed == "Y") {
         Glide.with(this.context)
-            .load(imgFile)
+            .load(R.drawable.success_stamp)
+            .override(R.dimen.complete_stamp_width * 2, R.dimen.complete_stamp_height)
             .into(this)
-    }
-
-
-    @BindingAdapter("runningResultDay")
-    @JvmStatic
-    fun TextView.setRunningResultDay(day: String){
-        val date = day.split("-")
-        val year = date.get(0)
-        val month = date.get(1).toInt()
-        val day = date.get(2).toInt()
-
-//        this.text = "${year}년 ${month}월 ${day}일 러닝"
-        this.text = resources.getString(R.string.running_result_day, year, month, day)
-    }
-
-    @BindingAdapter("runningDistance")
-    @JvmStatic
-    fun TextView.setRunnignDistance(distance: Int){
-        this.text = "${distance / 1000} km"
-    }
-
-    @BindingAdapter("runningTime")
-    @JvmStatic
-    fun TextView.setRunningTime(time: Int){
-        val hourInt = time / 3600
-        val minuteInt = (time / 60) % 60
-
-        var hour = hourInt.toString()
-        if(hourInt < 10){
-            hour = "0" + hour
-        }
-
-        var minute = minuteInt.toString()
-        if(minuteInt < 10){
-            minute = "0" + minute
-        }
-
-        this.text = "${hour} : ${minute}"
-    }
-
-    @BindingAdapter("runningAvgSpeed")
-    @JvmStatic
-    fun TextView.setRunningAvgSpeed(avgSpeed: Double){
-        this.text = "${round(avgSpeed * 10.0) / 10.0} km/h"
-    }
-
-    @BindingAdapter("runningStartTime", "runningEndTime")
-    @JvmStatic
-    fun TextView.setRunningStartToEndTime(startTime: String, endTime: String){
-        this.text = "$startTime ~ $endTime"
-    }
-
-    @BindingAdapter("runningResultCompleted")
-    @JvmStatic
-    fun ImageView.setRunningResultCompleted(completed: String){
-        if(completed == "Y"){
-            Glide.with(this.context)
-                .load(R.drawable.success_stamp)
-                .override(R.dimen.complete_stamp_width * 2, R.dimen.complete_stamp_height)
-                .into(this)
-        }else{
-            Glide.with(this.context)
-                .load(R.drawable.fail_stamp)
-                .override(R.dimen.complete_stamp_width * 2, R.dimen.complete_stamp_height)
-                .into(this)
-        }
+    } else {
+        Glide.with(this.context)
+            .load(R.drawable.fail_stamp)
+            .override(R.dimen.complete_stamp_width * 2, R.dimen.complete_stamp_height)
+            .into(this)
     }
 }
