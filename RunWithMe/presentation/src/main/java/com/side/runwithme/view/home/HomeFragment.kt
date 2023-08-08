338f19cdd0c29f168a17a982c385adb0d406460e
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-08-09 01:08:02 +0900

    fix : BearerException import 수정

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/home/HomeFragment.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/home/HomeFragment.kt
@@ -4,7 +4,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.seobaseview.base.BaseFragment
 import com.side.runwithme.R
 import com.side.runwithme.databinding.FragmentHomeBinding
-import com.side.runwithme.di.BearerException
+import com.side.runwithme.exception.bearer.BearerException
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
