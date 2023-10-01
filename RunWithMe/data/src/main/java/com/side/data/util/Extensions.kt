2f9ac675c6171802b1f21c6a5ed8e6d028b8e683
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-10-02 00:01:32 +0900

    feat : asResult 확장 함수 추가 (보일러 플레이트 제거)
    repository Impl에서 함수마다 emit(ResultType.Loading), catch emit(error) 할 필요 없도록 확장 함수 추가

--- RunWithMe/data/src/main/java/com/side/data/util/Extensions.kt
+++ RunWithMe/data/src/main/java/com/side/data/util/Extensions.kt
@@ -2,7 +2,16 @@ package com.side.data.util
 
 import com.side.domain.base.BaseResponse
 import com.side.domain.utils.ResultType
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.transform
 
 suspend fun <T> FlowCollector<ResultType<T>>.emitResultTypeError(throwable: Throwable) {
     this.emit(ResultType.Error(throwable))
@@ -19,3 +28,17 @@ suspend fun <T> FlowCollector<ResultType<T>>.emitResultTypeSuccess(data: T) {
 suspend fun <T> FlowCollector<ResultType<T>>.emitResultTypeFail(data: T) {
     this.emit(ResultType.Fail(data))
 }
+
+inline fun <T> Flow<T>.asResult(crossinline action: suspend (value: T) -> ResultType<T>): Flow<ResultType<T>> = this.transform {
+    emit(action(it))
+}.onStart {
+    emitResultTypeLoading()
+}.catch {
+    emitResultTypeError(it)
+}
+
+// datasource에서 사용하려 했지만 datasource 함수를 suspend로 모두 변경해야해서 생략 (datasource는 변경할만큼 귀찮은 코드가 아닌듯)
+//inline suspend fun <T> BaseResponse<T>.asResult(): Flow<BaseResponse<T>> =
+//    flow {
+//        emit(this@asResult)
+//    }
