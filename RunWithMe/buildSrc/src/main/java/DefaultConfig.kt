cdf91d6484a87544e4f15d90c661e6305e21849f
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-10-10 08:39:13 +0900

    gradle : minSdk 26으로 변경
    minSdk 26 미만에서는 LocalDate 사용 불가

--- RunWithMe/buildSrc/src/main/java/DefaultConfig.kt
+++ RunWithMe/buildSrc/src/main/java/DefaultConfig.kt
@@ -2,7 +2,7 @@ object DefaultConfig {
 
     const val TARGET_SDK = 33
     const val COMPILE_SDK = 33
-    const val MIN_SDK = 24
+    const val MIN_SDK = 26
 
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
