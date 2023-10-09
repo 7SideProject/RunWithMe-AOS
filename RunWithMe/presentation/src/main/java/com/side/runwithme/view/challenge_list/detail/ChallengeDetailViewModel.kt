fc379bcc3c46d6aaab2fb0242b8bfb886d9458c4
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-10-10 08:53:00 +0900

    yet :  ChallengeDetail 화면 Api 연결 확인, 가입, 해체 API 추가해야함
    isJoin, challenge 속성이 변경될때마다 challengeState를 변경하도록 combine 사용함

---
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/challenge_list/detail/ChallengeDetailViewModel.kt
@@ -0,0 +1,143 @@
+package com.side.runwithme.view.challenge_list.detail
+
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.side.domain.usecase.challenge.IsChallengeAlreadyJoinUseCase
+import com.side.domain.usecase.user.GetUserProfileUseCase
+import com.side.domain.utils.onError
+import com.side.domain.utils.onFailure
+import com.side.domain.utils.onSuccess
+import com.side.runwithme.model.ChallengeParcelable
+import com.side.runwithme.util.CHALLENGE_STATE
+import com.side.runwithme.util.MutableEventFlow
+import com.side.runwithme.util.asEventFlow
+import com.side.runwithme.util.onlyDateFormatter
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import javax.inject.Inject
+
+@HiltViewModel
+class ChallengeDetailViewModel @Inject constructor(
+    private val getUserProfileUseCase: GetUserProfileUseCase,
+    private val isChallengeAlreadyJoinUseCase: IsChallengeAlreadyJoinUseCase,
+) : ViewModel() {
+
+    private val _challenge = MutableStateFlow<ChallengeParcelable?>(null)
+    val challenge = _challenge.asStateFlow()
+
+    private val _managerName = MutableStateFlow<String>("")
+    val managerName = _managerName.asStateFlow()
+
+    private val isJoin = MutableStateFlow<Boolean>(false)
+
+    val challengeState = combine(isJoin, challenge){ isJoin, challenge ->
+
+        val dateEnd = challenge!!.dateEnd
+        val dateStart = challenge.dateStart
+        // 다음날로 지나갈 수도 있기 때문에 변경된 상황에서 측정하고자 함
+        val today = LocalDate.now().onlyDateFormatter()
+
+        if(dateEnd <= today){
+            CHALLENGE_STATE.END
+        }else if(isJoin && dateStart <= today){
+            CHALLENGE_STATE.START
+        }else if(isJoin && dateStart > today){
+            CHALLENGE_STATE.NOT_START_AND_ALEADY_JOIN
+        }else if(!isJoin && dateStart > today) {
+            CHALLENGE_STATE.NOT_START_AND_NOT_JOIN
+        }else {
+            CHALLENGE_STATE.NOTHING
+        }
+
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), CHALLENGE_STATE.NOTHING)
+
+    private val _ChallengeDetailEventFlow = MutableEventFlow<Event>()
+    val ChallengeDetailEventFlow = _ChallengeDetailEventFlow.asEventFlow()
+
+
+    fun setChallnege(challenge: ChallengeParcelable){
+        _challenge.value = challenge
+
+        getManagerProfile()
+    }
+
+    fun getManagerProfile(){
+        viewModelScope.launch(Dispatchers.IO) {
+            getUserProfileUseCase(challenge.value!!.managerSeq).collect {
+                it.onSuccess {
+                    _managerName.value = it.data?.nickname ?: ""
+                }.onFailure {
+
+                }.onError {
+                    Log.e("test123", "getManagerName: ", it)
+                }
+            }
+        }
+    }
+
+    fun isChallengeAlreadyJoin(){
+        viewModelScope.launch(Dispatchers.IO) {
+            isChallengeAlreadyJoinUseCase(challenge.value!!.seq).collect {
+                it.onSuccess {
+                    isJoin.value = it.data
+                }.onFailure {
+
+                }.onError {
+                    Log.e("test123", "isChallengeAlreadyJoin: ", it)
+                }
+            }
+        }
+    }
+
+    fun onClickButton(){
+        when(challengeState.value){
+            CHALLENGE_STATE.START -> {
+                goRunning()
+            }
+            CHALLENGE_STATE.NOT_START_AND_ALEADY_JOIN -> {
+                quitChallenge()
+            }
+            CHALLENGE_STATE.NOT_START_AND_NOT_JOIN -> {
+                joinChallenge()
+            }
+            else -> {
+
+            }
+        }
+
+    }
+
+    private fun goRunning(){
+        viewModelScope.launch {
+            /** 러닝 가능한지 확인 후 emit **/
+//            _ChallengeDetailEventFlow.emit(Event.Success())
+        }
+    }
+
+
+    /** api 구현해야함 **/
+    private fun joinChallenge(){
+        // join api 성공 시 ChallengeState AleadyJoin으로 변경
+    }
+
+    private fun quitChallenge(){
+        // quit api 성공 시 ChallengeState Not_Join으로 변경
+    }
+
+
+    sealed interface Event {
+        class Success : Event
+        class Fail : Event
+    }
+}
