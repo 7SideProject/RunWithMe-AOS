5219638e1edef2c4a048fa4a745516b2646ef3ea
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-10-10 10:02:44 +0900

    gradle : Target SDK 34로 변경

--- RunWithMe/buildSrc/src/main/java/DefaultConfig.kt
+++ RunWithMe/buildSrc/src/main/java/DefaultConfig.kt
@@ -1,8 +1,8 @@
 object DefaultConfig {
 
-    const val TARGET_SDK = 33
-    const val COMPILE_SDK = 33
-    const val MIN_SDK = 24
+    const val TARGET_SDK = 34
+    const val COMPILE_SDK = 34
+    const val MIN_SDK = 26
 
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
