d86e7f0fb77b7726dd11d068c0e589c8fa20f69e
DESKTOP-TKG5QQM\SangyeopHan <super7615@naver.com>
2023-10-19 22:13:23 +0900

    fix : 다음 버튼 글씨 안보이던 문제 수정

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/join/JoinBindingAdapter.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/join/JoinBindingAdapter.kt
@@ -3,18 +3,20 @@ package com.side.runwithme.view.join
 import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.BindingAdapter
 import com.side.runwithme.R
+import java.util.regex.Pattern
 
+const val emailValidation = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
 @BindingAdapter("enabledSendButton")
-fun AppCompatButton.setEnabledSendButton(phoneNumber: String){
-    if(phoneNumber.length == 11){
+fun AppCompatButton.setEnabledSendButton(email: String){
+    if(Pattern.matches(emailValidation,email)) {
         this.apply {
             isEnabled = true
-            setTextAppearance(R.style.StrongSmallButton)
+            setTextAppearance(R.style.enableSmallNextButton)
         }
     }else{
         this.apply {
             isEnabled = false
-            setTextAppearance(R.style.WeakSmallButton)
+            setTextAppearance(R.style.disableSmallNextButton)
         }
     }
 }
@@ -24,60 +26,27 @@ fun AppCompatButton.setEnabledVerifyButton(verifyNumber: String){
     if(verifyNumber.length == 6){
         this.apply {
             isEnabled = true
-            setTextAppearance(R.style.StrongSmallButton)
+            setTextAppearance(R.style.enableSmallNextButton)
         }
     }else{
         this.apply {
             isEnabled = false
-            setTextAppearance(R.style.WeakSmallButton)
+            setTextAppearance(R.style.disableSmallNextButton)
         }
     }
 }
-
-@BindingAdapter("enabledNextButton")
-fun AppCompatButton.setEnabledNextButton(complete: Boolean){
-    if(complete){
-        this.apply {
-            isEnabled = true
-            setTextAppearance(R.style.StrongButton)
-        }
-    }else{
-        this.apply {
-            isEnabled = false
-            setTextAppearance(R.style.WeakButton)
-        }
-    }
-}
-
-@BindingAdapter("enabledIDNextButton", "completePassword")
-fun AppCompatButton.setEnabledEmailNextButton(id: String, completePassword: Boolean){
-
-    if(id.length >= 4 && completePassword){
-        this.apply {
-            isEnabled = true
-            setTextAppearance(R.style.StrongButton)
-        }
-    }else{
-        this.apply {
-            isEnabled = false
-            setTextAppearance(R.style.WeakButton)
-        }
-    }
-}
-
-
 
 @BindingAdapter("enabledJoinButton")
 fun AppCompatButton.setEnabledJoinButton(allDone: Boolean){
     if(allDone){
         this.apply {
             isEnabled = true
-            setTextAppearance(R.style.StrongButton)
+            setTextAppearance(R.style.enableNextButton)
         }
     }else{
         this.apply {
             isEnabled = false
-            setTextAppearance(R.style.WeakButton)
+            setTextAppearance(R.style.disableNextButton)
         }
     }
 }
