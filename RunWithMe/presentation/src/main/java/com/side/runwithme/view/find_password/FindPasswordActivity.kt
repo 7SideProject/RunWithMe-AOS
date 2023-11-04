ed41f1e65e038794fc5b53b96491a89b510e8b13
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-11-04 23:40:55 +0900

    feat : 비밀번호 찾기 activity, viewmodel 로직

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/find_password/FindPasswordActivity.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/find_password/FindPasswordActivity.kt
@@ -1,13 +1,100 @@
 package com.side.runwithme.view.find_password
 
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
 import com.example.seobaseview.base.BaseActivity
 import com.side.runwithme.R
 import com.side.runwithme.databinding.ActivityFindPasswordBinding
+import com.side.runwithme.util.repeatOnStarted
+import com.side.runwithme.view.join.SendMail
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class FindPasswordActivity : BaseActivity<ActivityFindPasswordBinding>(R.layout.activity_find_password) {
+
+    private val findPasswordViewModel by viewModels<FindPasswordViewModel>()
+    private val delayTime = 30 * 1000L
+    private val mailSender = SendMail()
 
     override fun init() {
 
+        binding.apply {
+            findPasswordVM = findPasswordViewModel
+        }
+
+        initClickListener()
+
+        initViewModelCallbacks()
+    }
+
+    private fun initClickListener(){
+        var waitTime = 0L
+        var verifyDelayTime = 0L
+
+        binding.apply {
+            toolbar.setBackButtonClickEvent {
+                if(System.currentTimeMillis() - waitTime >= 1500){
+                    waitTime = System.currentTimeMillis()
+                    showToast(resources.getString(R.string.message_back_btn))
+                }else {
+                    finish()
+                }
+            }
+
+            btnSendNumber.setOnClickListener {
+                if (System.currentTimeMillis() - verifyDelayTime >= delayTime) {
+                    verifyDelayTime = System.currentTimeMillis()
+                    layoutVerify.visibility = View.VISIBLE
+                    hideKeyboard(etFindPasswordEmail)
+                    lifecycleScope.launch(Dispatchers.IO) {
+                        mailSender.sendSecurityCode(this@FindPasswordActivity, etFindPasswordEmail.text.toString())
+                    }
+                    showToast(resources.getString(R.string.message_send_mail))
+                } else {
+                    showToast(resources.getString(R.string.message_delay_send_mail))
+                }
+            }
+
+            btnVerify.setOnClickListener {
+                if(etVerifyNumber.text.toString() == mailSender.getEmailCode()){
+                    layoutFindPassword.visibility = View.VISIBLE
+                    findPasswordViewModel.verifyEmail()
+                }else{
+                    showToast(resources.getString(R.string.message_not_equal_verify))
+                }
+            }
+        }
+    }
+
+    private fun initViewModelCallbacks(){
+        findPasswordViewModel.apply {
+            repeatOnStarted {
+                findPasswordEventFlow.collectLatest {
+                    when(it){
+                        is FindPasswordViewModel.Event.Success -> {
+                            showToast(it.message)
+                            finish()
+                        }
+                        is FindPasswordViewModel.Event.Fail -> {
+                            showToast(it.message)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun hideKeyboard(editText: EditText){
+        val manager: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        manager.hideSoftInputFromWindow(editText.applicationWindowToken, 0)
     }
 
 }
--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/find_password/FindPasswordViewModel.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/find_password/FindPasswordViewModel.kt
@@ -1,13 +1,32 @@
 package com.side.runwithme.view.find_password
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.side.domain.usecase.user.ChangePasswordUseCase
+import com.side.runwithme.R
 import com.side.runwithme.util.MutableEventFlow
+import com.side.runwithme.util.PasswordVerificationType
 import com.side.runwithme.util.asEventFlow
+import com.side.runwithme.util.passwordValidation
+import com.side.runwithme.view.join.JoinViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class FindPasswordViewModel : ViewModel() {
+@HiltViewModel
+class FindPasswordViewModel @Inject constructor(
+    private val changePasswordUseCase: ChangePasswordUseCase
+) : ViewModel() {
 
-    val id: MutableStateFlow<String> = MutableStateFlow("")
+    val email: MutableStateFlow<String> = MutableStateFlow("")
 
     val password: MutableStateFlow<String> = MutableStateFlow("")
 
@@ -15,8 +34,65 @@ class FindPasswordViewModel : ViewModel() {
 
     val verifyNumber = MutableStateFlow<String>("")
 
+    private val _verifiedEmail = MutableStateFlow<String>("")
+    val verifiedEmail = _verifiedEmail.asStateFlow()
+
+    val doneState = combine(verifiedEmail, password, passwordConfirm) { verifiedEmail, password, passwordConfirm ->
+        if(verifiedEmail.isBlank() || password.isBlank() || passwordConfirm.isBlank()){
+            false
+        }
+        if(password != passwordConfirm){
+            false
+        }
+        true
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), false)
+
     private val _findPasswordEventFlow = MutableEventFlow<Event>()
     val findPasswordEventFlow = _findPasswordEventFlow.asEventFlow()
+
+
+    fun verifyEmail(){
+        _verifiedEmail.value = email.value
+    }
+
+    fun changePassword(){
+        if(email.value.isBlank()){
+            viewModelScope.launch {
+                _findPasswordEventFlow.emit(Event.Fail("이메일을 입력해주세요."))
+            }
+            return
+        }
+
+        if(email.value != verifiedEmail.value){
+            viewModelScope.launch {
+                _findPasswordEventFlow.emit(Event.Fail("인증한 이메일과 다릅니다."))
+            }
+            return
+        }
+
+        when(passwordValidation(password.value, passwordConfirm.value)){
+            PasswordVerificationType.LENGTH_ERROR -> {
+                viewModelScope.launch {
+                    _findPasswordEventFlow.emit(Event.Fail("비밀번호는 8~16자로 입력해주세요."))
+                }
+                return
+            }
+            PasswordVerificationType.NOT_EQUAL_ERROR -> {
+                viewModelScope.launch {
+                    _findPasswordEventFlow.emit(Event.Fail("비밀번호가 일치하지 않습니다."))
+                }
+                return
+            }
+            else -> {
+
+            }
+        }
+
+        /** API 요청 **/
+        viewModelScope.launch(Dispatchers.IO){
+            _findPasswordEventFlow.emit(Event.Success("비밀번호 변경 성공"))
+        }
+    }
 
     sealed class Event {
         data class Success(val message: String) : Event()
