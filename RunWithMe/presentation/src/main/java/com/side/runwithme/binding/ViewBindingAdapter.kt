6a8c6c1094dc74605cbfa176216adb8a8350348e
DESKTOP-TKG5QQM\SangyeopHan <super7615@naver.com>
2023-11-05 15:01:33 +0900

    feat : 누적 기록 호출 및 표기

--- RunWithMe/presentation/src/main/java/com/side/runwithme/binding/ViewBindingAdapter.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/binding/ViewBindingAdapter.kt
@@ -130,3 +130,16 @@ fun TextView.setWeight(weight: Int) {
 fun TextView.setPoint(point: Int) {
     this.text = "${point}P"
 }
+
+@BindingAdapter("setTotalRunningAvgSpeed")
+fun TextView.setTotalRunningAvgSpeed(avgSpeed: Double) {
+    this.text = "${avgSpeed} km/h"
+}
+
+@BindingAdapter("setTotalRunningTime")
+fun TextView.setTotalRunningAvgSpeed(totalTime: Int) {
+    val hour = totalTime / 60
+    val minute = totalTime % 60
+    this.text = "${hour}시간 ${minute}분"
+}
+
