3b6824e529c7506fea077aadfaca01804602fb34
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-11-11 15:24:42 +0900

    feat : Authenticator 추가 (refresh token 갱신 완료)

---
+++ RunWithMe/data/src/main/java/com/side/data/interceptor/AccessTokenAuthenticator.kt
@@ -0,0 +1,79 @@
+package com.side.data.interceptor
+
+import android.util.Log
+import com.side.data.datasource.datastore.DataStoreDataSource
+import com.side.data.datasource.token.TokenDataSource
+import com.side.domain.exception.BearerException
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AccessTokenAuthenticator @Inject constructor(
+    private val dataStoreDataSource: DataStoreDataSource,
+    private val tokenDataSource: TokenDataSource
+): Authenticator {
+
+    companion object {
+        private const val AUTHORIZATION = "authorization"
+    }
+
+    override fun authenticate(route: Route?, response: Response): Request? {
+        if(response.code == 401) {
+            try {
+                val refreshResponse = runBlocking {
+                    val refreshToken = dataStoreDataSource.getRefreshToken().first()
+                    tokenDataSource.refreshingToken(refreshToken)
+                }
+
+                if (refreshResponse.isSuccessful) {
+                    val newToken = getToken(refreshResponse)
+                    if (newToken.isBlank()) {
+                        throw BearerException()
+                    }
+
+                    val newRequest = response.request
+                        .newBuilder()
+                        .header(AUTHORIZATION, newToken)
+                        .build()
+
+                    return newRequest
+                } else {
+                    throw BearerException()
+                }
+            } catch (exception: Exception) {
+                throw BearerException()
+            }
+        }
+
+        return null
+    }
+
+    private fun getToken(response: retrofit2.Response<Any?>): String {
+        val allHeaders = response.headers()
+        val jwt = allHeaders.get("authorization") ?: ""
+        val refreshToken = allHeaders.get("set-cookie")?.let {
+            it.split("; ").get(0).split("=").get(1)
+        } ?: ""
+
+        /** refresh token 갱신되는지 확인해야함 **/
+        Log.d("test123", "authenticator : headers : ${allHeaders}")
+        Log.d("test123", "authenticator: jwt : ${jwt}")
+        Log.d("test123", "authenticator: refreshtoken : ${refreshToken}")
+
+        runBlocking {
+            saveToken(jwt, refreshToken)
+        }
+
+        return jwt
+    }
+
+    private suspend fun saveToken(jwt: String, refreshToken: String) {
+        dataStoreDataSource.saveToken(jwt, refreshToken)
+    }
+}
