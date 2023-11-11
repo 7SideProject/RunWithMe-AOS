cf2f9c897bbd8c09c8c222fdd24284a3c476983c
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-11-11 15:21:47 +0900

    fix : refreshtoken api 요청 시  form data로 보내도록 수정

--- RunWithMe/data/src/main/java/com/side/data/api/TokenApi.kt
+++ RunWithMe/data/src/main/java/com/side/data/api/TokenApi.kt
@@ -1,11 +1,17 @@
 package com.side.data.api
 
+import com.side.data.model.request.RefreshTokenRequest
+import com.side.domain.base.BaseResponse
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface TokenApi {
 
-    @POST("/token")
-    suspend fun refreshingToken(@Body refreshToken: String)
+    @FormUrlEncoded
+    @POST("token")
+    suspend fun refreshingToken(@Field("grantType") grantType: String, @Field("refreshToken") refreshToken: String): Response<Any?>
 
 }
