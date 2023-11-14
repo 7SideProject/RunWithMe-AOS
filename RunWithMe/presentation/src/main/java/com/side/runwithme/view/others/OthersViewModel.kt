708559b81ce94f72f0e3afd94c4e900e15d7482d
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-11-14 16:25:13 +0900

    feat : logout, 개인정보처리방침, 문의 구현

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/others/OthersViewModel.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/others/OthersViewModel.kt
@@ -4,24 +4,33 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
+import com.side.domain.model.User
+import com.side.domain.usecase.datastore.SaveTokenDataStoreUseCase
+import com.side.domain.usecase.datastore.SaveUserDataStoreUseCase
 import com.side.domain.usecase.user.DeleteUserUseCase
 import com.side.runwithme.util.MutableEventFlow
 import com.side.runwithme.util.asEventFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class OthersViewModel @Inject constructor(
-    private val deleteUserUseCase: DeleteUserUseCase
-): ViewModel() {
+    private val deleteUserUseCase: DeleteUserUseCase,
+    private val saveUserDataStoreUseCase: SaveUserDataStoreUseCase,
+    private val saveTokenDataStoreUseCase: SaveTokenDataStoreUseCase
+) : ViewModel() {
 
     private val _deleteUserEventFlow = MutableEventFlow<DeleteUserEvent>()
     val deleteUserEventFlow = _deleteUserEventFlow.asEventFlow()
 
-    fun deleteUser(){
+    private val _logoutEventFlow = MutableEventFlow<LogoutEvent>()
+    val logoutEventFlow = _logoutEventFlow.asEventFlow()
+
+    fun deleteUser() {
         viewModelScope.launch(Dispatchers.IO) {
             deleteUserUseCase().collectLatest {
                 it.onSuccess {
@@ -35,8 +44,32 @@ class OthersViewModel @Inject constructor(
         }
     }
 
+    fun logOut() {
+        viewModelScope.launch(Dispatchers.IO) {
+            saveTokenDataStoreUseCase("", "")
+            saveUserDataStoreUseCase(
+                User(
+                    seq = 0L,
+                    email = "",
+                    nickname = "",
+                    height = 0,
+                    weight = 0,
+                    point = 0
+                )
+            )
+
+            delay(500L)
+
+            _logoutEventFlow.emit(LogoutEvent.Success())
+        }
+    }
+
     sealed class DeleteUserEvent {
         class Success : DeleteUserEvent()
         class Fail : DeleteUserEvent()
+    }
+
+    sealed class LogoutEvent {
+        class Success : LogoutEvent()
     }
 }
