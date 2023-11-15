d134a8f38794a972620a97d355556ec51414f3c9
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-11-16 04:02:29 +0900

    feat : 자동 로그인 구현

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/login/LoginViewModel.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/login/LoginViewModel.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.side.domain.model.User
+import com.side.domain.usecase.datastore.GetJWTDataStoreUseCase
+import com.side.domain.usecase.datastore.GetPermissionCheckUseCase
+import com.side.domain.usecase.datastore.SavePermissionCheckUseCase
 import com.side.domain.usecase.user.LoginWithEmailUseCase
 import com.side.domain.utils.ResultType
 import com.side.runwithme.util.MutableEventFlow
@@ -17,7 +20,10 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val loginWithEmailUseCase: LoginWithEmailUseCase
+    private val loginWithEmailUseCase: LoginWithEmailUseCase,
+    private val savePermissionCheckUseCase: SavePermissionCheckUseCase,
+    private val getPermissionCheckUseCase: GetPermissionCheckUseCase,
+    private val getJWTDataStoreUseCase: GetJWTDataStoreUseCase
 ) : ViewModel() {
 
     val email: MutableStateFlow<String> = MutableStateFlow("")
@@ -26,6 +32,8 @@ class LoginViewModel @Inject constructor(
     private val _loginEventFlow = MutableEventFlow<Event>()
     val loginEventFlow get() = _loginEventFlow.asEventFlow()
 
+    private val _permissionEventFlow = MutableEventFlow<Boolean>()
+    val permissionEventFlow = _permissionEventFlow.asEventFlow()
 
     fun loginWithEmail() {
         val user = if(email.value.isBlank()) User("test@naver.com", "12341234") else User(email.value, password.value)
@@ -47,6 +55,33 @@ class LoginViewModel @Inject constructor(
                         Log.d("test123", "${it.exception.cause} ")
                     }
                     else -> {}
+                }
+            }
+        }
+    }
+
+    fun checkAutoLogin(){
+        viewModelScope.launch(Dispatchers.IO) {
+            getJWTDataStoreUseCase().collectLatest {
+                if(it.isNotBlank()){
+                    _loginEventFlow.emit(Event.Success())
+                }
+            }
+        }
+    }
+
+    fun savePermissionCheck(){
+        viewModelScope.launch(Dispatchers.IO) {
+            savePermissionCheckUseCase(true)
+        }
+    }
+
+    fun getPermissionCheck(){
+        viewModelScope.launch(Dispatchers.IO) {
+            getPermissionCheckUseCase().collectLatest {
+                if(!it){
+                    savePermissionCheck()
+                    _permissionEventFlow.emit(false)
                 }
             }
         }
