b2f1742bcef16618358c67ba327e059d83851c93
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-11-16 03:57:41 +0900

    feat : challenge 가입, 탈퇴 시 dialog로 확인 한번 더 요청

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/challenge_detail/ChallengeDetailFragment.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/challenge_detail/ChallengeDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.side.runwithme.view.challenge_detail
 
 import android.content.Intent
+import android.util.Log
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -8,6 +9,7 @@ import com.example.seobaseview.base.BaseFragment
 import com.side.runwithme.R
 import com.side.runwithme.databinding.FragmentChallengeDetailBinding
 import com.side.runwithme.model.ChallengeParcelable
+import com.side.runwithme.util.CHALLENGE_STATE
 import com.side.runwithme.util.GOAL_TYPE
 import com.side.runwithme.util.repeatOnStarted
 import com.side.runwithme.view.running.RunningActivity
@@ -45,7 +47,68 @@ class ChallengeDetailFragment :
             toolbarMain.setBackButtonClickEvent {
                 findNavController().popBackStack()
             }
+
+            btnOk.setOnClickListener {
+                when (challengeDetailViewModel.challengeState.value) {
+                    CHALLENGE_STATE.NOT_START_AND_ALEADY_JOIN -> {
+                        initChallengeDetailDialog(
+                            resources.getString(
+                                R.string.quit_challenge_content,
+                                challengeDetailViewModel.challenge.value!!.cost
+                            ), quitChallengeClickListener
+                        )
+                    }
+
+                    CHALLENGE_STATE.NOT_START_AND_NOT_JOIN -> {
+                        initChallengeDetailDialog(
+                            resources.getString(
+                                R.string.join_challenge_content,
+                                challengeDetailViewModel.challenge.value?.cost
+                            ), joinChallengeClickListener
+                        )
+                    }
+
+                    else -> {
+
+                    }
+                }
+            }
         }
+    }
+
+    private val passwordDialog = object : JoinChallengePasswordDialogClickListener {
+        override fun onClick(password: String) {
+            challengeDetailViewModel.joinChallenge(password)
+        }
+    }
+
+    private val quitChallengeClickListener = object : ChellengeDetailConfirmDialogClickListener {
+        override fun onClick() {
+            challengeDetailViewModel.quitChallenge()
+        }
+    }
+
+    private val joinChallengeClickListener = object : ChellengeDetailConfirmDialogClickListener {
+        override fun onClick() {
+            if (challengeDetailViewModel.challenge.value!!.password.isNullOrBlank()) {
+                challengeDetailViewModel.joinChallenge()
+                return
+            }
+            initCheckPasswordDialog()
+        }
+    }
+
+    private fun initCheckPasswordDialog(){
+        val dialog = JoinChallengePasswordDialog(passwordDialog)
+        dialog.show(childFragmentManager, "JoinChallengePasswordDialog")
+    }
+
+    private fun initChallengeDetailDialog(
+        content: String,
+        clickListener: ChellengeDetailConfirmDialogClickListener
+    ) {
+        val dialog = ChallengeDetailConfirmDialog(content, clickListener)
+        dialog.show(childFragmentManager, "ChallengeDetailConfirmDialog")
     }
 
     private fun initViewModelCallbacks() {
@@ -57,7 +120,7 @@ class ChallengeDetailFragment :
             challengeDetailViewModel.ChallengeDetailEventFlow.collect {
                 when (it) {
                     is ChallengeDetailViewModel.Event.Success -> {
-                        goRunning()
+//                        goRunning()
                         moveHomeFragment()
                     }
 
@@ -74,24 +137,24 @@ class ChallengeDetailFragment :
         }
     }
 
-    private fun goRunning() {
-        val intent = Intent(requireContext(), RunningActivity::class.java)
-        intent.apply {
-            val challenge = challengeDetailViewModel.challenge.value!!
-            val goalType =
-                if (challenge.goalType == GOAL_TYPE.DISTANCE.apiName) GOAL_TYPE.DISTANCE.ordinal else GOAL_TYPE.TIME.ordinal
-            putExtra("challengeSeq", challenge.seq)
-            putExtra("goalType", goalType)
-            putExtra("goalAmount", challenge.goalAmount)
-        }
-        startActivity(intent)
-    }
+//    private fun goRunning() {
+//        val intent = Intent(requireContext(), RunningActivity::class.java)
+//        intent.apply {
+//            val challenge = challengeDetailViewModel.challenge.value!!
+//            val goalType =
+//                if (challenge.goalType == GOAL_TYPE.DISTANCE.apiName) GOAL_TYPE.DISTANCE.ordinal else GOAL_TYPE.TIME.ordinal
+//            putExtra("challengeSeq", challenge.seq)
+//            putExtra("goalType", goalType)
+//            putExtra("goalAmount", challenge.goalAmount)
+//        }
+//        startActivity(intent)
+//    }
 
     private fun initChallenge(challenge: ChallengeParcelable) {
         challengeDetailViewModel.setChallnege(challenge)
     }
 
-    private fun moveHomeFragment(){
+    private fun moveHomeFragment() {
         findNavController().navigate(R.id.action_challengeDetailFragment_to_HomeFragment)
     }
 }
---
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/challenge_detail/JoinChallengePasswordDialog.kt
@@ -0,0 +1,27 @@
+package com.side.runwithme.view.challenge_detail
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import com.example.seobaseview.base.BaseDialogFragment
+import com.side.runwithme.R
+import com.side.runwithme.databinding.DialogJoinChallengePasswordBinding
+
+class JoinChallengePasswordDialog(private val joinChallengePasswordDialogClickListener: JoinChallengePasswordDialogClickListener) : BaseDialogFragment<DialogJoinChallengePasswordBinding>(
+    R.layout.dialog_join_challenge_password){
+
+    override fun init() {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.WHITE))
+
+        initClickListener()
+
+    }
+
+    private fun initClickListener(){
+        binding.apply {
+            tvPositive.setOnClickListener {
+                joinChallengePasswordDialogClickListener.onClick(etPasswd.text.toString())
+                dismiss()
+            }
+        }
+    }
+}
