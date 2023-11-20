4ceac23a2890fda8310d33f7cdb31a4b8891df0f
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-11-20 22:10:47 +0900

    feat : 프로필 수정 시, 이미지 수정 로직 추가

--- RunWithMe/presentation/src/main/java/com/side/runwithme/view/my_page/edit_profile/EditProfileFragment.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/view/my_page/edit_profile/EditProfileFragment.kt
@@ -1,17 +1,35 @@
 package com.side.runwithme.view.my_page.edit_profile
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
 import com.example.seobaseview.base.BaseFragment
 import com.side.runwithme.R
 import com.side.runwithme.databinding.FragmentEditProfileBinding
+import com.side.runwithme.util.BASE_URL
+import com.side.runwithme.util.GET_PROFILE_IMG
+import com.side.runwithme.util.USER
 import com.side.runwithme.util.repeatOnStarted
+import com.side.runwithme.util.resizeBitmapFormUri
 import com.side.runwithme.view.my_page.MyPageViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.ByteArrayOutputStream
 
 @AndroidEntryPoint
 class EditProfileFragment : BaseFragment<FragmentEditProfileBinding>(R.layout.fragment_edit_profile) {
@@ -36,8 +54,27 @@ class EditProfileFragment : BaseFragment<FragmentEditProfileBinding>(R.layout.fr
     private fun initClickListener(){
         binding.apply {
             btnModify.setOnClickListener {
+                setMultiPartBody()
                 myPageViewModel.editProfileRequest()
             }
+            ivUserProfile.setOnClickListener {
+                pickPhotoGallery()
+            }
+            ivEditPhoto.setOnClickListener {
+                pickPhotoGallery()
+            }
+        }
+    }
+
+    private fun setMultiPartBody(){
+        try {
+            if(myPageViewModel.profileImgUri.value != null){
+                val bitmap = resizeBitmapFormUri(myPageViewModel.profileImgUri.value!!,requireContext())
+                if(bitmap == null) return
+                myPageViewModel.setProfileImg(createMultiPart(createByteArray(bitmap)))
+            }
+        } catch (e : Exception){
+            e.printStackTrace()
         }
     }
 
@@ -55,6 +92,15 @@ class EditProfileFragment : BaseFragment<FragmentEditProfileBinding>(R.layout.fr
                 }
             }
         }
+
+        lifecycleScope.launch {
+            myPageViewModel.profileImgUri.collectLatest {
+                if(it != null) {
+                    binding.ivUserProfile.setImageURI(it)
+                }
+            }
+        }
+
     }
 
 
@@ -121,5 +167,40 @@ class EditProfileFragment : BaseFragment<FragmentEditProfileBinding>(R.layout.fr
                 }
             }
         }
+    }
+
+    private fun pickPhotoGallery() {
+        val photoIntent = Intent(Intent.ACTION_PICK)
+        photoIntent.type = "image/*"
+        photoIntent.putExtra("crop","true")
+        pickPhotoResult.launch(photoIntent)
+    }
+
+    private val pickPhotoResult : ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+
+        if(it.resultCode == Activity.RESULT_OK){
+            val uri = it.data?.data
+            myPageViewModel.setProfileImgUri(uri)
+
+        }
+    }
+
+    private fun createByteArray(bitmap: Bitmap): ByteArray{
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 10, outputStream)
+        return outputStream.toByteArray()
+    }
+
+    private fun createMultiPart(imageByteArray: ByteArray): MultipartBody.Part {
+        val requestFile = imageByteArray.toRequestBody("multipart/form-data".toMediaTypeOrNull())
+        return MultipartBody.Part.createFormData("image", "profile", requestFile)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("test123", "onDestroyView: d")
+        myPageViewModel.setProfileImg(null)
+        myPageViewModel.setProfileImgUri(null)
     }
 }
