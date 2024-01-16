9dee53dc111cf3ac251d363a73dcfc47df7c77d4
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2024-01-16 20:56:23 +0900

    fix : 수치 변경

--- RunWithMe/presentation/src/main/java/com/side/runwithme/mapper/CoordinatesMapper.kt
+++ RunWithMe/presentation/src/main/java/com/side/runwithme/mapper/CoordinatesMapper.kt
@@ -6,8 +6,8 @@ import com.side.runwithme.model.CoordinatesParcelable
 fun List<CoordinatesParcelable>.mapperToCoordinate() : List<Coordinate> = this.run {
     return this.map {
         Coordinate(
-            (it.latitude * 100_000).toInt(),
-            (it.longitude * 100_000).toInt()
+            (it.latitude * 1_000_000).toInt(),
+            (it.longitude * 1_000_000).toInt()
         )
     }
 }
