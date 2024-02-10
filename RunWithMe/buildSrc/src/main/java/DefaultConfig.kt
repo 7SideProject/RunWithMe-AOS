1374c9ad6267c9e7dfb74776b0720611ec2ac9b7
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2024-02-10 15:56:56 +0900

    hotfix : ui fix

--- RunWithMe/buildSrc/src/main/java/DefaultConfig.kt
+++ RunWithMe/buildSrc/src/main/java/DefaultConfig.kt
@@ -4,8 +4,8 @@ object DefaultConfig {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 26
 
-    const val VERSION_CODE = 6
-    const val VERSION_NAME = "1.20"
+    const val VERSION_CODE = 7
+    const val VERSION_NAME = "1.21"
 
     const val HILT_TEST_RUNNER = "com.side.runwithme.di.HiltTestRunner"
 
